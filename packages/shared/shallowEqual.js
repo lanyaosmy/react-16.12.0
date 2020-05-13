8a204755c2a94eac5e6c3ef460d76b0fe1287dba
mengyao.sun <mengyao.sun@qunar.com>
2020-05-13 12:16:35 +0800

    first commit

---
+++ packages/shared/shallowEqual.js
@@ -0,0 +1,68 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow
+ */
+
+import is from './objectIs';
+
+/**
+ * 
+ *  Object.is() 判断两个值是否相同。如果下列任何一项成立，则两个值相同：
+    两个值都是 undefined
+    两个值都是 null
+    两个值都是 true 或者都是 false
+    两个值是由相同个数的字符按照相同的顺序组成的字符串
+    两个值指向同一个对象
+    两个值都是数字并且
+    都是正零 +0
+    都是负零 -0
+    都是 NaN
+    都是除零和 NaN 外的其它同一个数字
+ */
+const hasOwnProperty = Object.prototype.hasOwnProperty;
+
+/**
+ * Performs equality by iterating through keys on an object and returning false
+ * when any key has values which are not strictly equal between the arguments.
+ * Returns true when the values of all keys are strictly equal.
+ */
+function shallowEqual(objA: mixed, objB: mixed): boolean {
+  if (is(objA, objB)) {
+    return true;
+  }
+  // 判空
+  if (
+    typeof objA !== 'object' ||
+    objA === null ||
+    typeof objB !== 'object' ||
+    objB === null
+  ) {
+    return false;
+  }
+
+  const keysA = Object.keys(objA);
+  const keysB = Object.keys(objB);
+
+  // 判断两个对象中的key的数量是否相同
+  if (keysA.length !== keysB.length) {
+    return false;
+  }
+
+  // 判断两个对象中的key和value是否相同
+  for (let i = 0; i < keysA.length; i++) {
+    if (
+      !hasOwnProperty.call(objB, keysA[i]) ||
+      !is(objA[keysA[i]], objB[keysA[i]])
+    ) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+export default shallowEqual;
